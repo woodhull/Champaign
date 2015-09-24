45142bccd291085f01eeaf6933c449cd81b14622
Eric Boersma <eric.boersma@gmail.com>
2015-09-24 10:27:54 -0400

    Adding queue push to Action Creation.

--- app/services/manage_action.rb
+++ app/services/manage_action.rb
@@ -6,6 +6,14 @@ class ManageAction
   def create
     Action.find_or_create_by( action_user: action_user, campaign_page: page ) do |action|
       action.form_data = @params
+
+      # Push the action to our queue which will send it to ActionKit.
+      merged_params = @params.merge({email: @params[:action_email_address], slug: page.slug})
+      send_to_ak = {type: 'action', params: merged_params}
+      ChampaignQueue.push(send_to_ak)
+
+      # Return action since the UI needs it.
+      action
     end
   end
 
